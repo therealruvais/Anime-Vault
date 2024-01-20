174b57c5abd1c9d345f4355036e21d1d09b91537
therealruvais <ruvais666@gmail.com>
2024-01-20 08:06:02 +0530

    first commit

---
+++ app/action.tsx
@@ -0,0 +1,18 @@
+"use server"
+
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
+const fetchAnime = async (page:Number) => {
+    const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`) 
+    const data = await response.json();
+    console.log(data);
+    return data.map((item: AnimeProp, index: number) => (
+      <AnimeCard
+        key={item.id}
+        anime={item}
+        index={index}
+      />
+    ));
+}
+
+export default fetchAnime
